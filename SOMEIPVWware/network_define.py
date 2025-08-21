ddde9871e81c5b916d0fce71ec381481399e00dc
LIU <2859528910@qq.com>
2025-08-21 15:30:59 +0800

    Add files via upload

---
+++ SOMEIPVWware/network_define.py
@@ -0,0 +1,11 @@
+
+# 虚拟机端配置
+class EthParameter:
+    sd_network_card = "ens33"
+    sd_ip = "224.224.224.245"
+    server_network_card = "ens33"
+    server_ip = "192.168.137.129"
+    client_ip = "192.168.20.86"  # 主机IP
+    sd_port = 30490
+    producer_port = 30500
+    consumer_prot = 30501
